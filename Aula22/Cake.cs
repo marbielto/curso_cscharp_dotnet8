51371ff4d9686e2304876d5294d969506408e346
Marcos Gabriel Barreto <77794251+marbielto@users.noreply.github.com>
2025-05-16 19:35:28 -0300

    Instância de classes

---
+++ Aula22/Cake.cs
@@ -0,0 +1,14 @@
+﻿namespace Aula22
+{
+    internal class Cake
+    {
+        //Atributos
+
+        public int Id;
+        public string Name;
+        public string Description;
+        public decimal Value;
+        public bool IsFilling;
+
+    }
+}
